f4e78b245c47d3c935b1ffd26c4b0797641bdfe9
EldarMan <elDarkFlame@gmail.com>
2020-03-04 17:37:25 +0500

    Added babel js processing

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,23 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist")
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all"
-    }
-  },
+
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist"
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
