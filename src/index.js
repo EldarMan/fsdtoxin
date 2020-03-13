94dd2867a7a949c2a675ecbb57d14b8e9f8726ec
EldarMan <elDarkFlame@gmail.com>
2020-03-13 19:15:24 +0500

    Created bullet component

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ import "./components/checkbox/checkbox";
 import "./components/radio/radio";
 import "./components/toggle/toggle";
 import "./components/like/like";
+import "./components/bullet/bullet";
 import "./components/rate/rate";
