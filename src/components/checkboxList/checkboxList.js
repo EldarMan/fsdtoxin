3d5ec51ec83c3dc3eff45ef5ef21ea4668f5128a
EldarMan <elDarkFlame@gmail.com>
2020-04-26 09:50:13 +0500

    Component Checkbox List -> Expandable Checkbox List

--- src/components/checkboxList/checkboxList.js
+++ src/components/checkboxList/checkboxList.js
@@ -1,1 +1,15 @@
 import "./checkboxList.scss";
+const baseClass = "checkbox-list";
+document.querySelectorAll(".checkbox-list").forEach((element) => {
+  const title = document.querySelector(`.${baseClass}__title`);
+  const list = document.querySelector(`.${baseClass}__list`);
+  title.addEventListener("click", (e) => {
+    if (list.classList.contains(`${baseClass}__list_collapsed`)) {
+      list.classList.remove(`${baseClass}__list_collapsed`);
+      list.classList.add(`${baseClass}__list_expanded`);
+    } else {
+      list.classList.remove(`${baseClass}__list_expanded`);
+      list.classList.add(`${baseClass}__list_collapsed`);
+    }
+  });
+});
